72c5aaf925faf740f6e9eb2dfb436e6f7dcb3bf4
kevkimbu <kevkim@bu.edu>
2023-11-09 03:46:31 -0500

    Set up basic pages and create a navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route index element={<Home />} />
         
